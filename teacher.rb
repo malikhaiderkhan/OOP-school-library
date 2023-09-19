d7c4f314c5b585e3d87ac0a7ce0d857d9e2a7484
Haider <malikhaiderkhan81@gmail.com>
2023-09-19 20:30:54 +0500

    use the colon notation to assign the parent_permission

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require './person'
 
 class Teacher < Person
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
