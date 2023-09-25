bc2e89069113c232234dc2a15138e80e49ea929e
Haider <malikhaiderkhan81@gmail.com>
2023-09-26 01:52:06 +0500

    add options and run code to main file

--- lib/app.rb
+++ lib/app.rb
@@ -4,14 +4,7 @@ require_relative 'person'
 require_relative 'rental'
 require_relative 'student'
 require_relative 'teacher'
-CHOICES = {
-  1 => :list_books,
-  2 => :list_people,
-  3 => :create_person,
-  4 => :create_book,
-  5 => :create_rental,
-  6 => :list_rentals_for_person
-}.freeze
+
 class App
   def initialize
     @books = []
@@ -112,32 +105,7 @@ class App
     end
   end
 
-  def run
-    loop do
-      display_menu
-      choice = gets.chomp.to_i
-      exit if choice == 7
-      if CHOICES.key?(choice)
-        send(CHOICES[choice])
-      else
-        puts 'Invalid choice. Please try again.'
-      end
-    end
-  end
-
   private
-
-  def display_menu
-    puts ' '
-    puts 'Please choose an option by entering a number:'
-    puts '1. List all books'
-    puts '2. List all people'
-    puts '3. Create a person'
-    puts '4. Create a book'
-    puts '5. Create a rental'
-    puts '6. List all rentals for a given person ID'
-    puts '7. Quit'
-  end
 
   def display_books
     puts 'List of Books:'
