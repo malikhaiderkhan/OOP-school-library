53bad04abe16ca361f5ed71a1f0066b16380a33d
devendra-alt <devendramulewa171@gmail.com>
2023-09-25 19:40:27 +0530

    Change lib file location

--- capitalize_decorator.rb
+++
@@ -1,7 +0,0 @@
-require './base_decorator'
-
-class CapitalizeDecorator < BaseDecorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
--- nameable.rb
+++
@@ -1,5 +0,0 @@
-class Nameable
-  def correct_name
-    raise NotImplementedError, 'method not implemented yet.'
-  end
-end
