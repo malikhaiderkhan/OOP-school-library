1e8392986f12477736976901c2d97351d073d006
devendra-alt <devendramulewa171@gmail.com>
2023-09-26 21:26:14 +0530

    Refectored person class and subclasses

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,7 @@ require_relative 'trimmer_decorator'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age, :rentals
+  attr_accessor :name, :age, :rentals, :parent_permission
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
