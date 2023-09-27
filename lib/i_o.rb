649175e49d54822284b205baf60fbdf5e93b3b7d
devendra-alt <devendramulewa171@gmail.com>
2023-09-27 20:44:08 +0530

    Refactor to_json and from_json

--- lib/i_o.rb
+++ lib/i_o.rb
@@ -4,7 +4,7 @@ require_relative 'teacher'
 require_relative 'rental'
 require_relative 'book'
 require_relative 'person'
-require 'pry'
+
 class IOFILE
   def file_exist?(name)
     File.exist?(name) && File.size(name).positive?
@@ -14,18 +14,18 @@ class IOFILE
     object_container = []
     if file_exist?(name)
       JSON.parse(File.read(name)).each do |element|
-      if name == 'people.json'
-
-        # binding.pry
-        object_container << if element['type'] == 'Student'
-                              Student.new(element['age'], element['name'], element['parent_permission'])
-                            else
-                              Teacher.new(element['age'], element['specialization'], element['name'])
-                            end
-      elsif name == 'rentals.json'
-        book = Book.new(element['book'].title, element['book'].author)
-        person = Person.new(element['person'].id, element['person'].name, element['person'].age, element['person'].parent_permission)
-        object_container << Rental.new(element['date'], book, person)
+        if name == 'people.json'
+          object_container << if element['type'] == 'Student'
+                                Student.new(element['age'], element['parent_permission'], element['name'])
+                              else
+                                Teacher.new(element['age'], element['specialization'], element['name'])
+                              end
+        elsif name == 'rentals.json'
+          book = Book.new(element['book']['title'], element['book']['author'])
+          person = Person.new(element['person']['name'],
+                              element['person']['age'])
+          object_container << Rental.new(element['date'], book, person)
+        end
       end
     end
     object_container
@@ -39,25 +39,31 @@ class IOFILE
     rentals_data
   end
 
+  def person_stream(element)
+    rentals_data_stream = from_rentals(element.rentals)
+    if element.is_a?(Student)
+      { type: 'Student', age: element.age, name: element.name,
+        parent_permission: element.parent_permission,
+        rentals: rentals_data_stream }
+    else
+      { type: 'Teacher', age: element.age, specialization:
+                      element.specialization, name: element.name,
+        rentals: rentals_data_stream }
+    end
+  end
+
   def to_json(data_container, name)
     data_stream_object_container = []
     File.open(name, 'w') do |file|
       data_container.each do |element|
-      if name == 'people.json'
-
-        rentals_data_stream = from_rentals(element.rentals)
-        if element.is_a?(Student)
-          data_stream_object_container.push({ type: 'Student', age: element.age, name: element.name,
-                                              parent_permission: element.parent_permission,
-                                              rentals: rentals_data_stream })
-        else
-          data_stream_object_container.push({ type: 'Teacher', age: element.age, specialization: element.specialization,
-                                              name: element.name, rentals: rentals_data_stream })
+        if name == 'people.json'
+          data_stream_object_container.push(person_stream(element))
+        elsif name == 'rentals.json'
+          book = { author: element.book.author, title: element.book.title }
+          person = { id: element.person.id, title: element.person.name, age: element.person.age,
+                     parent_permission: element.person.parent_permission }
+          data_stream_object_container.push({ date: element.date, book: book, person: person })
         end
-      elsif name == 'rentals.json'
-        book = { element.book.author, element.book.title }
-        person = { element.person.id, element.person.name, element.person.age, element.person.parent_permission }
-        data_stream_object_container.push({ date:element.date, book:book, person:person })
       end
       file.write(data_stream_object_container.to_json)
     end
