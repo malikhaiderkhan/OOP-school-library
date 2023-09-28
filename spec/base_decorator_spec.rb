7768b494e54c5d6a1e14a5709308b0026ac4a2f6
Haider <malikhaiderkhan81@gmail.com>
2023-09-29 00:54:19 +0500

    fix offenses

--- spec/base_decorator_spec.rb
+++ spec/base_decorator_spec.rb
@@ -1,15 +1,15 @@
 require_relative '../lib/base_decorator'
 
 RSpec.describe BaseDecorator do
-    describe '#correct_name' do
+  describe '#correct_name' do
     it 'returns the correct name' do
-        decorated_object = double('Decorated object')
-        allow(decorated_object).to receive(:correct_name).and_return('Haider')
+      decorated_object = double('Decorated object')
+      allow(decorated_object).to receive(:correct_name).and_return('Haider')
 
-        decorator = BaseDecorator.new(decorated_object)
-        result = decorator.correct_name
+      decorator = BaseDecorator.new(decorated_object)
+      result = decorator.correct_name
 
-        expect(result).to eq('Haider')
+      expect(result).to eq('Haider')
     end
-end
+  end
 end
