1d94d3682c24dd6edff21654806edba6c2a7fa57
Haider <malikhaiderkhan81@gmail.com>
2023-09-28 23:51:00 +0500

    fix offenses

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -6,16 +6,16 @@ RSpec.describe Person do
   describe '#initialize' do
     it 'creates a person with default values' do
       person = Person.new(25)
-      
+
       expect(person.name).to eq('Unknown')
       expect(person.parent_permission).to be_truthy
       expect(person.age).to eq(25)
       expect(person.rentals).to be_empty
     end
-    
+
     it 'creates a person with custom values' do
       person = Person.new(30, 'John Doe', parent_permission: false)
-      
+
       expect(person.name).to eq('John Doe')
       expect(person.parent_permission).to be_falsy
       expect(person.age).to eq(30)
@@ -27,7 +27,7 @@ RSpec.describe Person do
     context 'when of age' do
       it 'can use services' do
         person = Person.new(18)
-        
+
         expect(person.can_use_services?).to be_truthy
       end
     end
@@ -35,7 +35,7 @@ RSpec.describe Person do
     context 'when not of age with parent permission' do
       it 'can use services' do
         person = Person.new(16, 'Devendra', parent_permission: true)
-        
+
         expect(person.can_use_services?).to be_truthy
       end
     end
@@ -43,7 +43,7 @@ RSpec.describe Person do
     context 'when not of age without parent permission' do
       it 'cannot use services' do
         person = Person.new(16, 'Blue', parent_permission: false)
-        
+
         expect(person.can_use_services?).to be_falsy
       end
     end
@@ -52,7 +52,7 @@ RSpec.describe Person do
   describe '#correct_name' do
     it 'returns the correct name' do
       person = Person.new(25, 'John Doe')
-      
+
       expect(person.correct_name).to eq('John Doe')
     end
   end
@@ -61,9 +61,9 @@ RSpec.describe Person do
     it 'creates a rental and adds it to the person' do
       person = Person.new(25, 'John Doe')
       book = Book.new('Hitman', 'Haider')
-      
+
       rental = person.add_rental(book, '2023-09-28')
-      
+
       expect(person.rentals).to include(rental)
       expect(rental.person).to eq(person)
     end
