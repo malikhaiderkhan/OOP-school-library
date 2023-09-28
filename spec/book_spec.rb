6e7728d514fb0bf01a5bd5515d6ed4758ebc90c5
Haider <malikhaiderkhan81@gmail.com>
2023-09-28 19:38:44 +0500

    add initialize method test for book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -3,6 +3,25 @@ require_relative '../lib/person'
 require_relative '../lib/rental'
 
 RSpec.describe Book do
+  describe '#initialize' do
+    it 'creates a new book with title and author' do
+      title = 'Subcontinent'
+      author = 'Haider'
+
+      book = Book.new(title, author)
+
+      expect(book.title).to eq(title)
+      expect(book.author).to eq(author)
+    end
+
+    it 'initializes rentals as an empty array' do
+      book = Book.new('Title', 'Author')
+
+      expect(book.rentals).to be_an(Array)
+      expect(book.rentals).to be_empty
+    end
+  end
+
   describe '#add_rental' do
     it 'adds a rental to the book' do
       book = Book.new('Subcontinent', 'Haider')
