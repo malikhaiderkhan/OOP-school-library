94d9f30ef57595eaa1ab2b8c6c1f4ec8dedc4865
devendra-alt <devendramulewa171@gmail.com>
2023-09-29 09:21:50 +0530

    Fixed linter issue

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -7,9 +7,7 @@ RSpec.describe Rental do
     it 'creates a rental' do
       book = Book.new('Subcontinent', 'Haider')
       person = Person.new(26, 'David')
-
       rental = Rental.new('2023-09-28', book, person)
-
       expect(rental.book).to eq(book)
       expect(rental.person).to eq(person)
     end
--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Teacher do
   describe '#initialize' do
     it 'creates a teacher with default values' do
       teacher = Teacher.new(26, 'Ruby')
-
       expect(teacher.name).to eq('Unknown')
       expect(teacher.parent_permission).to be_truthy
       expect(teacher.age).to eq(26)
@@ -13,7 +12,6 @@ RSpec.describe Teacher do
 
     it 'creates a teacher with custom values' do
       teacher = Teacher.new(26, 'Ruby', 'John', parent_permission: true)
-
       expect(teacher.name).to eq('John')
       expect(teacher.parent_permission).to be_truthy
       expect(teacher.age).to eq(26)
@@ -24,7 +22,6 @@ RSpec.describe Teacher do
   describe '#can_use_service?' do
     it 'can always use services' do
       teacher = Teacher.new(26, 'Ruby')
-
       expect(teacher.can_use_services?).to be_truthy
     end
   end
